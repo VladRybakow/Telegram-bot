0353a11d0fbd521356f01ce36b091e4611f5045d
Анатолий Жмышенко <gggame184@gmail.com>
2021-06-21 12:38:06 +0300

    Добавьте файлы проекта.

---
+++ Telegram bot/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Telegram.Bot;
+
+namespace TelegramBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TelegramBotClient bot = new TelegramBotClient("1899796180:AAG0g_YFvoIj0s9C15PC4XUxe0UOyQKuJx4");
+
+            bot.OnMessage += (s, arg) =>
+            {
+                Console.WriteLine($"{arg.Message.Chat.FirstName}: {arg.Message.Text}");
+                bot.SendTextMessageAsync(arg.Message.Chat.Id, $"You say: {arg.Message.Text}");
+            };
+
+            bot.StartReceiving();
+
+            Console.ReadKey();
+        }
+    }
+}
